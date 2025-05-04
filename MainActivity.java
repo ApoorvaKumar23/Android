81300d41458a7b9a84eac3f921280d31aa5dc18a
ApoorvaKumar23 <165682723+ApoorvaKumar23@users.noreply.github.com>
2025-05-04 22:23:07 +0530

    Exp2

--- MainActivity.java
+++ MainActivity.java
@@ -1,39 +1,25 @@
+package com.example.experiment2;
 
-package com.example.experiment3;
-
-import android.os.Bundle;
+import android.os.*;
 import android.view.*;
 import android.widget.*;
-import androidx.activity.EdgeToEdge;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.*;
 
 public class MainActivity extends AppCompatActivity {
-    EditText etUsername, etPassword;
-    Button btnLogin;
-    DBHelper dbHelper;
+
+    Button button1; // Declare button
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        etUsername = findViewById(R.id.etUsername);
-        etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        dbHelper = new DBHelper(this);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+
+        button1 = findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String user = etUsername.getText().toString();
-                String pass = etPassword.getText().toString();
-
-                if (dbHelper.login(user, pass)) {
-                    Toast.makeText(MainActivity.this, "Login Success", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(MainActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                }
+                Toast.makeText(MainActivity.this, "Button Clicked!", Toast.LENGTH_SHORT).show();
             }
         });
     }
